a14eb3e79863069890776bfc198e6e7e347c27c3
NaviVirgolok <119007703+NaviVirgolok@users.noreply.github.com>
2022-11-25 02:48:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ weather.temperature = {
 }
 // App consts and var 
 const KELVIN = 273;    
-const key = "82005d27a116c2880c8f0fcb866998a0";
+const key = "3f27136dfb221565feb9357377d20544";
+// old 82005d27a116c2880c8f0fcb866998a0
+//my 3f27136dfb221565feb9357377d20544
 
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
